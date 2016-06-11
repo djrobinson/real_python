e5c0a1627d0fc325a6f3facf143a3bff16931726
Danny Robinson <daniel@djrobinson.me>
2016-06-11 12:17:24 -0600

    String concat, slicing

---
+++ strings.py
@@ -0,0 +1,24 @@
+my_string = "stringy"
+
+string_len = len(my_string)
+
+print(string_len)
+
+
+string1 = "blah"
+
+string2 = "boop"
+
+concatter = string1 + string2
+
+print(concatter)
+
+print("hello","world")
+
+select_char = "muffins"
+
+print(select_char[2])
+
+print(select_char[0:3])
+
+print(select_char[4:])
