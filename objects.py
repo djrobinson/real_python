14a061b3932c56ebf67973d0f58c67ecad053213
Danny Robinson <daniel@djrobinson.me>
2016-06-11 12:27:54 -0600

    Basic methods & objects

---
+++ objects.py
@@ -0,0 +1,13 @@
+loud_voice = "can you hear me?"
+
+print(loud_voice.upper())
+
+user_input = input("Hey, what's up?")
+
+print("you said:", user_input)
+
+user_pw = input("Tell me your password: ")
+
+first_letter = user_pw[0].upper()
+
+print(first_letter)
