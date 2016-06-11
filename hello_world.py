31f650fa690e636966c3dff1a9ea538d7a21bec5
Danny Robinson <daniel@djrobinson.me>
2016-06-11 12:05:59 -0600

    String exercises

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+phrase = "Hello, world!"
+
+print(phrase)
+
+'''
+This should be a comment
+'''
+sentence = '''lorem ipsumljadskjfsldjfajsdflkjasd;fjalksjfdlajsdklfjasl
+kljasdlkfjasjdfasdf'''
+
+print(sentence)
+
+multiline = "testing a multiline string \
+and going on to the next line"
+
+print(multiline)
