07151e7a2c55ba0b5bd9a2e632fc508f20400e4e
Danny Robinson <daniel@djrobinson.me>
2016-06-11 14:07:07 -0600

    String interpolation

---
+++ string_funcamentals.py
@@ -0,0 +1,24 @@
+my_number = "12"
+
+print(int(my_number))
+
+print(float(my_number))
+
+my_float = "12.0"
+
+#print(int(my_float))
+
+print(str(12) + "12")
+
+name = "couch"
+num_heads = 7
+num_arms = 8
+
+print("{} has {} heads and {} arms".format(name, num_heads, num_arms))
+
+
+print("{2} has {1} heads and {0} arms".format(name, num_heads, num_arms))
+
+print("{name} has {num_heads} heads and {num_arms} arms".format(
+    name="Zaphod", num_heads=2, num_arms=3
+))
